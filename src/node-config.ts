7739094f21c6a3645cb086b7e16babef88b51b9c
Steve Ognibene <steve.ognibene@outlook.com>
2017-08-20 11:04:16 -0400

    fix explicit path on node-config

--- src/node-config.ts
+++ src/node-config.ts
@@ -2,8 +2,10 @@ import * as path from 'path';
 import * as fs from 'fs';
 
 class Config implements IConfig {
-    constructor(staticContentPath = "../WizardsContent/") {
-        const staticContentFullName = path.join(__dirname, staticContentPath, "static-content.json");
+    constructor(staticContentPath?: string) {
+        const staticContentFullName = staticContentPath === undefined ?
+            path.join(__dirname, "../WizardsContent/static-content.json") :
+            path.join(staticContentPath, "static-content.json");
         const content = fs.readFileSync(staticContentFullName, {encoding: "utf8"});
         const data: IConfigFile = JSON.parse(content);
         this.Races = data.Races;
--- tests/node-config.test.ts
+++ tests/node-config.test.ts
@@ -1,6 +1,25 @@
+import * as path from 'path';
 import NodeConfig from "../src/node-config";
 
-test("Races loaded from config file.", () => {
+test("Races loaded from config file with no path specified.", () => {
+    expect.hasAssertions();
     const sut = new NodeConfig();
-    expect(sut.Classes).not.toBeUndefined();
+    validateConfig(sut);
 });
+
+test("Races loaded from config file with path specified.", () => {
+    expect.hasAssertions();
+    const explicitPath = path.join(__dirname, "../WizardsContent/").replace(/\\/g, "/");
+    expect(explicitPath).toMatch(/\/WizardsContent\/$/);
+    const sut = new NodeConfig(explicitPath);
+    validateConfig(sut);
+});
+
+function validateConfig(config: IConfig) {
+    expect(config.Classes).not.toBeUndefined();
+    expect(config.Classes.length).toBeGreaterThanOrEqual(12);
+    expect(config.Classes).toContain("Druid");
+    expect(config.Races).not.toBeUndefined();
+    expect(config.Races.length).toBeGreaterThanOrEqual(9);
+    expect(config.Races).toContain("Dwarf");
+}
